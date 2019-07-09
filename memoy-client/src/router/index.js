e5a7ada24c69c6aa4c6c654838ff70805df6f667
yecx <1214025698@qq.com>
2019-07-09 21:44:26 +0800

    Revert "Revert "菜单栏以及项目雏形搭建""
    This reverts commit a5921b3e

--- memoy-client/src/router/index.js
+++ memoy-client/src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Index from '../components/index'
+import Index from '../components/accounting/index'
 Vue.use(Router)
 
 export default new Router({
